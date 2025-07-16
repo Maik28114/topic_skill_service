eba2a2557f54e7dda9b619a585459addb10f4f20
Maik28114 <bbb-maik@web.de>
2025-07-16 10:33:53 +0200

    PLPG-API-4: Implemented the basic flask app.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Topic and Skill Service!'
+
+
+if __name__== '__main__':
+    app.run(debug=True, port=5000)
+    
+
+
+
+
