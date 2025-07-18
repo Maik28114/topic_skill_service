8549e48fc49f3aad6b90cdc231f8207f7b7eb2f3
Maik28114 <bbb-maik@web.de>
2025-07-18 16:20:17 +0200

    PLPG-API-11-12: Implemented post topics/skills endpoint.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import os
-from flask import Flask, jsonify
+import uuid
+from flask import Flask, jsonify, request 
 from data_manager import JsonDataManager
 
 app = Flask(__name__)
@@ -17,7 +18,8 @@ def hello_world():
 
 
 
-
+# ------------------- GET METHODS -----------------
+# --------------------- topics ---------------------
 @app.route('/topics', methods=['GET'])
 def get_topics():
     topics = data_manager.read_data(TOPICS_FILE)
@@ -30,7 +32,7 @@ def get_topic_id(id):
     if topic: 
         return jsonify(topic)
     return jsonify({"[ERROR]": "Topic ID not found"}), 404
-
+# --------------------- skills ---------------------
 
 @app.route("/skills", methods=["GET"])
 def get_skills():
@@ -44,6 +46,46 @@ def get_skills_id(id):
     if skill:
         return jsonify(skill)
     return jsonify({"[ERROR]": "Skill ID not found"}), 404
+
+# ------------------- POST METHODS -----------------
+# --------------------- topics ---------------------
+
+@app.route('/topics', methods=['POST'])
+def create_topic():
+    new_topic_data = request.json
+    if not new_topic_data or 'name' not in new_topic_data or 'description' not in new_topic_data:
+        return jsonify({"[ERROR]":"'Name' or 'description' not in POST"}),400
+    new_topic_id = str(uuid.uuid4())
+    new_topic = {
+        "id": new_topic_id,
+        "name": new_topic_data['name'],
+        "description": new_topic_data['description']
+    }
+    topics = data_manager.read_data(TOPICS_FILE)
+    topics.append(new_topic)
+
+    data_manager.write_data(TOPICS_FILE, topics)
+    return jsonify(new_topic), 201
+
+@app.route('/skills', methods=['POST'])
+def create_skills():
+    new_skills_data = request.json
+    if not new_skills_data or 'name' not in new_skills_data or 'topicId' not in new_skills_data:
+        return jsonify({"ERROR]":"'Name' or 'topicId' not in POST"}),400
+    new_skills_id = str(uuid.uuid4())
+    new_skills = {
+        "id": new_skills_id,
+        "name": new_skills_data['name'],
+        "topicId": new_skills_data['topicId'],
+        "difficulty": new_skills_data['difficulty']
+
+    }
+    skills = data_manager.read_data(SKILLS_FILE)
+    skills.append(new_skills)
+
+    data_manager.write_data(SKILLS_FILE, skills)
+    return jsonify(new_skills), 201
+
 
 
 
